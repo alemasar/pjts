1801845580453558c3ffadf56ffe794b2d6d7117
Alemasar <alemasar@gmail.com>
2024-06-13 03:46:19 +0200

    https configuration

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import path from 'path';
 import config from './src/pjts/pjts.config'
 import addComponentsPlugin from './vite/plugins/vite-plugin-add-components'
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default defineConfig({
-  plugins: [addComponentsPlugin(config)],
+  server: {
+    port: 3000,
+    https: true,
+  },
+  plugins: [basicSsl(), addComponentsPlugin(config)],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
