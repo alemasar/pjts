a18b5ab651a4906f535dba0096af81f603f3b9a5
Alemasar <alemasar@gmail.com>
2024-07-18 22:27:27 +0200

    MVP Router near to be completed. There is a bug to analize and fix.

--- src/framework/cat-page.ts
+++ src/framework/cat-page.ts
@@ -1,49 +1,23 @@
+const pageLinks: { obj: HTMLAnchorElement; handler: (evt: any) => void; }[] = []
 class CatPage extends HTMLElement {
   templateId: string;
   constructor() {
     super();
-    const url = new URL(document.location.href)
-    const pathname = url.pathname.split('/')
-    const event = new CustomEvent("page-loaded");
-    this.templateId = 'indexTemplate'
-    
-    pathname.shift()
-    if (pathname.length > 0){
-      if (pathname[0] !== '') {
-        this.templateId = pathname[0] + 'Template'
-      }
-    }
-    console.log("PATHNAME LENGTH", document.getElementById(this.templateId))
-    console.log("PATHNAME SPLIT", url.pathname.split('/'))
-    this.innerHTML = document.getElementById(this.templateId)?.innerHTML as string
+      const event = new CustomEvent("page-loaded");
+    this.templateId = ''
+    this.changePageTemplateFromLocation()
     window.addEventListener('popstate', (evt) => {
+      const event = new CustomEvent("page-loaded");
       console.log('ENTRO')
-      event.preventDefault()
-      this.innerHTML = document.getElementById(this.templateId)?.innerHTML as string;
+      evt.preventDefault()
+      this.changePageTemplateFromLocation()
       document.dispatchEvent(event);
     })
+    document.dispatchEvent(event);
   }
 
   connectedCallback() {
-    const links = this.querySelectorAll("a")
-    const event = new Event("page-loaded");
-    links.forEach((l)=> {
-      l.addEventListener('click', (event) => {
-        const link = event.target as HTMLAnchorElement
-        const hrefValue = link.getAttribute("href") as string
-        const url = hrefValue.split("/").pop()
-        event.preventDefault()
-        console.log("Custom element added to page.", link.getAttribute("href"));
-        history.pushState({}, 'newUrl', '/' + url);
-        if (url === '') {
-          this.templateId = 'indexTemplate'
-        } else {
-          this.templateId = url + 'Template'
-        }
-        this.innerHTML = document.getElementById(this.templateId)?.innerHTML as string;
-      }, false)
-    })
-    document.dispatchEvent(event);
+
   }
 
   disconnectedCallback() {
@@ -57,7 +31,82 @@ class CatPage extends HTMLElement {
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     console.log(`Attribute ${name} has changed.`);
   }
+  
+  attachEventClickLinks() {
+    const links = this.querySelectorAll("a")
+    this.removeLinksClickEvent()
+    links.forEach((l)=> {
+      const returnObj = {
+        obj: l,
+        handler: this.clickHandler.bind(this),
+      }
+      l.addEventListener('click', this.clickHandler.bind(this) , false);
+      pageLinks.push(returnObj);
+    })
+  }
 
+  removeLinksClickEvent() {
+    pageLinks.forEach((pl) => {
+      console.log("REMOVE EVENT")
+      pl.obj.removeEventListener("click", pl.handler, false)
+    })
+    pageLinks.splice(0)
+  }
 
+  clickHandler(evt: any) {
+    console.log("ENTRO EN HANDLER CLICK", this.linkHandler)
+    return this.linkHandler(evt)
+  }
+
+  linkHandler(evt: any) {
+    // const event = new Event("page-loaded");
+    const link = evt.target as HTMLAnchorElement
+    const hrefValue = link.getAttribute("href") as string
+    const url = hrefValue.split("/").pop() as string
+    evt.preventDefault()
+    console.log("LINK HANDLER", link.getAttribute("href"));
+    this.changePageWithUrl(url)
+    // document.dispatchEvent(event);
+  }
+
+  getPathFromUrl() {
+    console.log('LOCATION HREF::::::',document.location.href)
+    const url = new URL(document.location.href)
+    const pathname = url.pathname.split('/')
+    let goToUrl = ''
+   console.log(url)
+    pathname.shift()
+    if (pathname.length > 0){
+      if (pathname[0] !== '') {
+        goToUrl = pathname[0]
+      }
+    }
+    return goToUrl
+  }
+
+  changePageTemplateFromLocation() {
+    const goToUrl = this.getPathFromUrl()
+    this.templateId = 'indexTemplate'
+
+    console.log("URL TO GO changePageTemplateFromLocation::::::::", history.state)
+    if (goToUrl !== '') {
+      this.templateId = goToUrl + 'Template'
+    }
+    history.pushState({}, '', '/' + goToUrl);
+    this.innerHTML = document.getElementById(this.templateId)?.innerHTML as string
+    this.attachEventClickLinks()
+  }
+  changePageWithUrl(url: string) {
+    const urlToGo = url.replace('/', '')
+    this.templateId = 'indexTemplate'
+
+    if (url !== '') {
+      this.templateId = urlToGo + 'Template'
+    }
+    history.pushState({}, '', '/' + urlToGo);
+    console.log("URL TO GO changePageWithUrl:::::::", history.state)
+    this.innerHTML = document.getElementById(this.templateId)?.innerHTML as string
+    this.attachEventClickLinks()
+  }
 }
-customElements.define("cat-page", CatPage);
+export default CatPage
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import "virtual:components";
 import { changeByNameValue } from "@framework/template-components/data-binding-component";
-import "@framework/cat-page";
+import CatPage from "@framework/cat-page";
 
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('page-loaded', (event) => {
-    console.log(event)
+    console.log('PAGE LOADED EVENT:::::::::::', event)
     setTimeout(()=> {
       changeByNameValue('DemoFirstTag', 'hello.message', 'BYE.... BYE..... GUAY');
     }, 1000)
   }, false)
+// },false)
+document.addEventListener('DOMContentLoaded', () => {
+  customElements.define("cat-page", CatPage);
 },false)
