80e86c856214e2036aafed1a4db3f172a6ca611a
Alemasar <alemasar@gmail.com>
2024-10-03 11:24:31 +0200

    Another refactor CatParseTemplates

--- src/cat/vite-plugins/plugin-helpers/CatParseTemplates.js
+++ src/cat/vite-plugins/plugin-helpers/CatParseTemplates.js
@@ -5,79 +5,83 @@ const getImportGapRegExp = /#import-id="(.|[\s\S])*?"/g
 const breaklinesRegExp = /\r?\n|\r|\n/g
 const getRequestGapRegExp = /#request "(.|[\s\S])*?"/g
 const getImportTemplateRegExp = /#import "(.|[\s\S])*?"/g
-class CatParseTemplates {
-  constructor() {
-    this.parsedGaps = new Map()
-    this.catGaps = new Map()
-    this.template = ''
-    this.catTemplates = new Map()
+
+const parseTemplates = (importTemplates, catTemplates) => {
+  importTemplates.forEach((it) => {
+    const parsedImportTemplates = it.split(breaklinesRegExp)
+    const importId = it.match(getImportGapRegExp)[0].trim().split(/=/g)[1].replace(/"/g, '')
+    catTemplates.set(importId, parsedImportTemplates)
+  })
+  return catTemplates
+}
+
+const parseRouteGaps = (defaultGaps, catGaps) => {
+  let routeName = {}
+  let splittedGaps = []
+  defaultGaps.forEach((dg) => {
+    splittedGaps = dg.split(breaklinesRegExp)
+    if (dg.includes('cat-gap=') === true) {
+      const arrayRoutes = splittedGaps[0].
+        replace(`<template cat-gap="`, '').
+        replace('"', '').
+        replace('>', '').
+        replace(/'/g,'"')
+      routeName = JSON.parse(`{"routes": ${arrayRoutes}}`)
+    }
+  })
+  routeName.routes.forEach((rn) => {
+    catGaps.set(rn, splittedGaps)
+  })
+  return catGaps
+}
+
+const parseImportTemplates = (line, resultArray, catTemplates) => {
+  const gapLinePos = line.indexOf('#import')
+  if (gapLinePos !== -1) {
+    const importId = line.trim().split(' ')[1].replace(/"/g, '')
+    resultArray = resultArray.concat(catTemplates.get(importId))
   }
+  return resultArray
+}
+
+const parseGaps = (gapLinesMap, catTemplates) => {
+  const resultMap = new Map()
+  let resultArray = []
+  for (let [key, gapLines] of gapLinesMap.entries()) {
+    gapLines.forEach((line) => {
+      resultArray = parseImportTemplates(line, resultArray, catTemplates)
+    })
+    resultMap.set(key, resultArray.join(''))
+    resultArray.splice(0)
+  }
+  return resultMap
+}
+
+
+class CatParseTemplates {
+  constructor() {}
   parseMultipleTemplates(templates) {
+    let catGaps = new Map()
+    let parsedGaps = new Map()
+    let catTemplates = new Map()
     templates.forEach((template) => {
       if (template.length > 0) {
         const importTemplates = template.match(templateImportRegExp)
         const routeGaps = template.match(getRoutesGapRegExp)
         const defaultGaps = template.match(templateGapRegExp)
         if (importTemplates !== null) {
-          this.parseTemplates(importTemplates)
+          catTemplates = parseTemplates(importTemplates, catTemplates)
         }
         if (routeGaps !== null) {
-          this.parseRouteGaps(defaultGaps)
+          catGaps = parseRouteGaps(defaultGaps, catGaps)
         } else if (defaultGaps !== null && defaultGaps.length > 0) {
           const splittedGaps = template.split(breaklinesRegExp)
-          this.catGaps.set('index', splittedGaps)
+          catGaps.set('index', splittedGaps)
         }
       }
     })
-    this.parsedGaps = this.parseGaps(this.catGaps)
-    return this.parsedGaps
-  }
-  parseTemplates(importTemplates) {
-    importTemplates.forEach((it) => {
-      const parsedImportTemplates = it.split(breaklinesRegExp)
-      const importId = it.match(getImportGapRegExp)[0].trim().split(/=/g)[1].replace(/"/g, '')
-      this.catTemplates.set(importId, parsedImportTemplates)
-    })
-    return this.catTemplates
-  }
-  parseImportTemplates(line, resultArray) {
-    const gapLinePos = line.indexOf('#import')
-    if (gapLinePos !== -1) {
-      const importId = line.trim().split(' ')[1].replace(/"/g, '')
-      resultArray = resultArray.concat(this.catTemplates.get(importId))
-    }
-    return resultArray
-  }
-  parseRouteGaps(defaultGaps) {
-    let routeName = {}
-    let splittedGaps = []
-    defaultGaps.forEach((dg) => {
-      splittedGaps = dg.split(breaklinesRegExp)
-      if (dg.includes('cat-gap=') === true) {
-        const arrayRoutes = splittedGaps[0].
-          replace(`<template cat-gap="`, '').
-          replace('"', '').
-          replace('>', '').
-          replace(/'/g,'"')
-        routeName = JSON.parse(`{"routes": ${arrayRoutes}}`)
-      }
-    })
-    routeName.routes.forEach((rn) => {
-      this.catGaps.set(rn, splittedGaps)
-    })
-    return this.catGaps
-  }
-  parseGaps(gapLinesMap) {
-    const resultMap = new Map()
-    let resultArray = []
-    for (let [key, gapLines] of gapLinesMap.entries()) {
-      gapLines.forEach((line) => {
-        resultArray = this.parseImportTemplates(line, resultArray, this.catTemplates)
-      })
-      resultMap.set(key, resultArray.join(''))
-      resultArray.splice(0)
-    }
-    return resultMap
+    parsedGaps = parseGaps(catGaps, catTemplates)
+    return parsedGaps
   }
 }
 
