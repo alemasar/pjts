ffcedb8907c4f90454cb0a94629cc400aab0d399
Alemasar <alemasar@gmail.com>
2024-10-06 03:45:37 +0200

    Refactor of parse scripts class

--- src/cat/vite-plugins/plugin-helpers/CatParseScripts.js
+++ src/cat/vite-plugins/plugin-helpers/CatParseScripts.js
@@ -4,47 +4,62 @@ const getImportScriptRegExp = /#import-id="(.|[\s\S])*?"/g
 const catGapInstructionName = `#cat-gap="`
 const importInstructionName = `#import-id="`
 
+const getScriptRouteImport = (idandroute, parsedScripts, scriptCode) => {
+  let catGapRoutes = ''
+  let importIdTemplate = ''
+  let catRouteObject = {}
+
+  idandroute.forEach((ir) => {
+    if (ir.match(getCatGapRouteScriptRegExp) !== null) {
+      catGapRoutes = ir.replace(catGapInstructionName, '').replace('"', '').replace(/'/g,'"')
+    }
+    if (ir.match(getImportScriptRegExp) !== null) {
+      importIdTemplate = ir.replace(importInstructionName, '').replace(`"`, '')
+    }
+  })
+  catRouteObject = JSON.parse(`{"routes": ${catGapRoutes}}`)
+  console.log(typeof catRouteObject)
+  if (catRouteObject.routes.length > 0) {
+    catRouteObject.routes.forEach((cro) => {
+      parsedScripts.set(cro, new Map())
+      if (importIdTemplate !== '') {
+        parsedScripts.get(cro).set(importIdTemplate, scriptCode)
+      } else {
+        parsedScripts.get(cro).set('default', scriptCode)
+      }
+    })
+  }
+  return parsedScripts
+}
+
+const getScriptImport = (idandroute, parsedScripts, scriptCode) => {
+  const importIdTemplate = idandroute[0].replace(importInstructionName, '').replace(`"`, '')
+  parsedScripts.set('index', new Map())
+  parsedScripts.get('index').set(importIdTemplate, scriptCode)
+  return parsedScripts
+}
+
+const getScriptIndexDefault = (scriptCode, parsedScripts) => {
+  parsedScripts.set('index', new Map())
+  parsedScripts.get('index').set('default', scriptCode)
+  return parsedScripts
+}
+
 class CatParseScripts {
   constructor() {}
   parseMultipleScripts(scripts) {
-    const parsedScripts = new Map()
+    let parsedScripts = new Map()
     if (scripts.length > 0) {
       scripts.forEach((s) => {
         const splittedScript = s.split(breaklinesRegExp)
         const scriptCode = splittedScript.join('\n')
         const idandroute = splittedScript[0].replace(`<script`, '').replace(`>`, '').trim().split(' ')
         if (splittedScript[0].match(getCatGapRouteScriptRegExp) !== null && splittedScript[0].match(getImportScriptRegExp) !== null) {
-          let catGapRoutes = ''
-          let importIdTemplate = ''
-          let catRouteObject = {}
-
-          idandroute.forEach((ir) => {
-            if (ir.match(getCatGapRouteScriptRegExp) !== null) {
-              catGapRoutes = ir.replace(catGapInstructionName, '').replace('"', '').replace(/'/g,'"')
-            }
-            if (ir.match(getImportScriptRegExp) !== null) {
-              importIdTemplate = ir.replace(importInstructionName, '').replace(`"`, '')
-            }
-          })
-          catRouteObject = JSON.parse(`{"routes": ${catGapRoutes}}`)
-          console.log(typeof catRouteObject)
-          if (catRouteObject.routes.length > 0) {
-            catRouteObject.routes.forEach((cro) => {
-              parsedScripts.set(cro, new Map())
-              if (importIdTemplate !== '') {
-                parsedScripts.get(cro).set(importIdTemplate, scriptCode)
-              } else {
-                parsedScripts.get(cro).set('default', scriptCode)
-              }
-            })
-          }
+          parsedScripts = getScriptRouteImport(idandroute, parsedScripts, scriptCode)
         } else if (splittedScript[0].match(getImportScriptRegExp) !== null) {
-          const importIdTemplate = idandroute[0].replace(importInstructionName, '').replace(`"`, '')
-          parsedScripts.set('index', new Map())
-          parsedScripts.get('index').set(importIdTemplate, scriptCode)
+          parsedScripts = getScriptImport(idandroute, parsedScripts, scriptCode)
         } else {
-          parsedScripts.set('index', new Map())
-          parsedScripts.get('index').set('default', scriptCode)
+          parsedScripts = getScriptIndexDefault(scriptCode, parsedScripts)
         }
       })
       console.log('CATPARSESCRIPTS::::', parsedScripts)
